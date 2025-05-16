de9a5b46e47df85fdaa5a06b5151fa9b638c1ab9
Wes Lee <weslee.qb@gmail.com>
2025-05-17 02:36:09 +0800

    Add initial package documentation for Wet Bulb Temperature Analysis

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Wet Bulb Temperature Analysis Package
+====================================
+This package provides tools for analyzing wet-bulb temperature in Singapore
+and its relationship to climate variables and greenhouse gases.
+"""
